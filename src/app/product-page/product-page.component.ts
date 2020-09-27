7ba671e9b74c54548e75f312d57b5bea7702c6cd
Tajudeen Woye <woyetajudeen@gmail.com>
2020-09-27 02:59:11 +0100

    Completed module

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../product.service";
+import { Album } from "../album";
 
 @Component({
-  selector: 'app-product-page',
-  templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.css']
+  selector: "app-product-page",
+  templateUrl: "./product-page.component.html",
+  styleUrls: ["./product-page.component.css"],
 })
 export class ProductPageComponent implements OnInit {
-
-  constructor() { }
+  albumInfo: Album;
+  constructor(private _productService: ProductService) {}
 
   ngOnInit() {
+    this._productService
+      .getAlbum(1)
+      .subscribe((response) => (this.albumInfo = response));
   }
-
 }
