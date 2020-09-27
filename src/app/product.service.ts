b08b9028097acab25ccafb2a986e272800b94679
Tajudeen Woye <woyetajudeen@gmail.com>
2020-09-27 03:54:23 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,11 +3,12 @@ import { Http, Response } from "@angular/http";
 import "rxjs/add/operator/map";
 import { Album } from "./album";
 import { Observable } from "rxjs/Observable";
+import { Product } from "./product";
 
 @Injectable()
 export class ProductService {
   private _albumUrl = "../assets/album.json";
-  private _http: any;
+  private _productsUrl = "../assets/products.json";
 
   constructor(private _http: Http) {}
 
@@ -15,5 +16,11 @@ export class ProductService {
     return this._http
       .get(this._albumUrl)
       .map((response) => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http
+      .get(this._productsUrl)
+      .map((response) => <Product[]>response.json());
   }
 }
